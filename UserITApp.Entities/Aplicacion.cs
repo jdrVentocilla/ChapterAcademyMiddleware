5c5f0c56f499f418c9075946b66dae7b6b9d0093
drventocilla <66335401+drventocilla@users.noreply.github.com>
2023-06-22 20:44:53 -0500

    feat: Implementacion de capas iniciales

---
+++ UserITApp.Entities/Aplicacion.cs
@@ -0,0 +1,16 @@
+﻿namespace UserITApp.Entities
+{
+    public class Aplicacion
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public Aplicacion(Guid id, string nombre, string descripcion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+        }
+    }
+}
